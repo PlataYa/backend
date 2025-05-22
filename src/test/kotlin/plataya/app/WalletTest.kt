e817dcd831295ec0acbbd23b14a37141458ca0bc
NachoCor2007 <incorrearodriguez@mail.austral.edu.ar>
2025-05-22 14:31:31 -0300

    First approach

--- src/test/kotlin/plataya/app/WalletTest.kt
+++ src/test/kotlin/plataya/app/WalletTest.kt
@@ -1,16 +1,27 @@
 package plataya.app
 
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import plataya.app.dto.WalletDTO
+import plataya.app.service.WalletService
 
 class WalletTest {
     @Test
-    fun correctCreationOfWallet() {
+    @DisplayName("Wallet should be created correctly")
+    fun test_1() {
         val walletMock = WalletDTO("mail@mail.com", 123456789, 1863.4F)
 
         assertEquals("mail@mail.com", walletMock.mail)
         assertEquals(123456789, walletMock.cvu)
         assertEquals(1863.4F, walletMock.balance)
+    }
+
+    @Test
+    @DisplayName("Wallet service should create a wallet correctly")
+    fun test_2() {
+        val service = WalletService()
+
+        val createdWallet = service.createWallet
     }
 }
